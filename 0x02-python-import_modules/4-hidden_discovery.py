c1eff3d162426173ca1abd4c8b1933df3145eae1
siyabonga <siyabongakhumalo602@gmail.com>
2024-01-19 03:00:31 -0800

    4-hidden_discovery.py

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import hidden_4
+
+def print_names():
+    names = dir(hidden_4)
+    for name in sorted(names):
+        if not name.startswith('__'):
+            print(name)
+
+if __name__ == "__main__":
+    print_names()
