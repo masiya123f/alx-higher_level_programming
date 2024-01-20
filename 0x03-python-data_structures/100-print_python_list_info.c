50bcc4f436e5e79958f62ace9233eab3f7a29483
siyabonga <siyabongakhumalo602@gmail.com>
2024-01-20 06:44:38 -0800

    test

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,46 +1,19 @@
 #include <Python.h>
+#include <stdio.h>
 
 void print_python_list_info(PyObject *p)
 {
-    Py_ssize_t size = PyList_Size(p);
+    Py_ssize_t size, i;
     PyObject *item;
-    int i;
 
-    printf("Size of the Python List = %ld\n", size);
-    printf("Allocated = %ld\n", PyList_GET_SIZE(p));
+    size = PyList_Size(p);
+    printf("[*] Size of the Python List = %ld\n", size);
+
+    printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
 
     for (i = 0; i < size; i++)
     {
-        item = PyList_GET_ITEM(p, i);
-        printf("Element %d: ", i);
-
-        if (PyString_CheckExact(item))
-            printf("str\n");
-        else if (PyInt_CheckExact(item))
-            printf("int\n");
-        else if (PyFloat_CheckExact(item))
-            printf("float\n");
-        else if (PyTuple_CheckExact(item))
-            printf("tuple\n");
-        else if (PyList_CheckExact(item))
-            printf("list\n");
-        else if (PyDict_CheckExact(item))
-            printf("dict\n");
-        else if (PyBytes_CheckExact(item))
-            printf("bytes\n");
-        else if (PyUnicode_CheckExact(item))
-            printf("unicode\n");
-        else if (PySet_CheckExact(item))
-            printf("set\n");
-        else if (PyDict_CheckExact(item))
-            printf("dict\n");
-        else if (PySlice_CheckExact(item))
-            printf("slice\n");
-        else if (PyBool_CheckExact(item))
-            printf("bool\n");
-        else if (PyNone_CheckExact(item))
-            printf("None\n");
-        else
-            printf("Unknown type\n");
+        item = PyList_GetItem(p, i);
+        printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
     }
 }
