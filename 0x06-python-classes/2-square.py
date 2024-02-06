e5ed0dabfccca1204f8ac132a5ad475fa4a1049b
siyabonga <siyabongakhumalo602@gmail.com>
2024-02-06 08:54:15 -0800

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
+""" defines a square """
+
+
 class Square:
+    """ square with private instance attribute size """
+
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
+        """
+        Args:
+            size: size of square
+        """
+
+        if type(size) is int:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
         else:
-            self.__size = size
+            raise TypeError('size must be an integer')
