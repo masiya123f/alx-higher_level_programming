a1187c6f759c45e2e1308ee207c88e5785a23a15
siyabonga <siyabongakhumalo602@gmail.com>
2024-02-20 09:12:20 -0800

    0-lookup.py

--- 0x0B-python-input_output/0-lookup.py
+++ 0x0B-python-input_output/0-lookup.py
@@ -1,19 +1,4 @@
 #!/usr/bin/python3
-# This line is called a shebang. It tells the system that this script should be run with Python 3.
-
-"""
-Contains the lookup function
-"""
-# This is a docstring at the module level. It provides a brief description of what the module contains.
-
-def lookup(obj):
-    # This line defines a function named 'lookup' that takes one argument, 'obj'.
-
-    """
-    returns a list of available attributes and methods of an object
-    """
-    # This is a docstring at the function level. It explains what the function does.
-
-    return dir(obj)
-    # This line returns a list of the names of all attributes and methods that are available for the object 'obj'.
-    # It uses the built-in Python function 'dir()'.
+def read_file(filename=""):
+    with open(filename, 'r', encoding='utf-8') as f:
+        print(f.read(), end="")
